4779f9331e57c1a03fcdbee83321f9e1741d8d66
Marianne Kenney <mkenney@ad-mac.local>
2018-06-06 15:34:58 -0400

    Initial Commit

---
+++ SmashMobile_v5UITests/SmashMobile_v5UITests.swift
@@ -0,0 +1,36 @@
+//
+//  SmashMobile_v5UITests.swift
+//  SmashMobile_v5UITests
+//
+//  Created by Marianne Kenney on 6/6/18.
+//  Copyright © 2018 SmashFly. All rights reserved.
+//
+
+import XCTest
+
+class SmashMobile_v5UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
